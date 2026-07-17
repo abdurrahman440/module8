f522aefe471db15ceb51a07454f671f176e6131b
abdurrahman440 <abdurrahman16017@gmail.com>
2026-07-18 00:49:35 +0600

    HW

---
+++ hw.js
@@ -0,0 +1,293 @@
+// let price1=100;
+// let price2=500;
+
+// if(price1==price2){
+//     console.log("price is equal");
+// }
+// else{
+//     console.log("price is not equal")
+// }
+
+// if(price1===price2){
+//     console.log("price is equal");
+// }
+// else{
+//     console.log("price is not equal")
+// }
+
+// if(price1!=price2){
+//     console.log("price is not  equal");
+// }
+// else{
+//     console.log("price is  equal")
+// }
+
+// if(price1!==price2){
+//     console.log("price is equal");
+// }
+// else{
+//     console.log("price is not equal")
+// }
+
+// if(price1>price2){
+//     console.log("price1 is greater");
+// }
+// else{
+//     console.log("price2 is greater")
+// }
+
+// if(price1<price2){
+//     console.log("price2 is greater");
+// }
+// else{
+//     console.log("price1 is greater")
+// }
+
+// if(price1>=price2){
+//     console.log("price1 is greater or equal");
+// }
+// else{
+//     console.log("price2 is greater or equal")
+// }
+
+// if(price1<=price2){
+//     console.log("price2 is greater or equal");
+// }
+// else{
+//     console.log("price1 is greater or equal")
+// }
+
+// ---------------------------q2-----------------------------
+
+
+
+//  ==(Loose equality,eta value miliye dekhe and dorkar hole data 
+//    change kore dekhe.)
+// if(5=="5"){
+//     console.log("it is true")
+// }
+// else{
+//     console.log("it is not true ")
+// }
+
+// ===(Strict equality,eta value and data type both check kore)
+// if(5==="5"){
+//     console.log("it is true")
+// }
+// else{
+//     console.log("it is not true ")
+// }
+
+// --------------question 3---------------------------
+
+// let isRaining=true;
+// isRaining=false;
+
+
+// if(isRaining=false){
+
+//     console.log("Take an umbrella ")
+// }
+// else{
+//     console.log("dont take the umbrella")
+// }
+
+
+// ----------------question 4 ----------------
+
+// if(stock==0) proshno korse stock ki 0?.
+// if(stock) proshno korse stock er truthy naki falsy.
+// JavaScript a 0->false,1-->true,10->true,"" khali string->false
+
+// let stock=0;
+
+// if(stock==0){
+// console.log("out of stock")
+// }
+
+// --------------question 5-------------------------
+
+// let number="jkh";
+
+// if(number>0){
+//     console.log("number is positive")
+// }
+// else if(number<0){
+//     console.log("number is negative ")
+// }
+// else if(number==0){
+//     console.log("not positive ,not negative")
+// }
+// else{
+//     console.log("Error")
+// }
+
+// ----------------quetion 6---------------
+
+// const year=2026;
+
+// if(year%4===0 && year&100!==0||year%400===0){
+// console.log("leap year")
+
+// }
+// else{
+//     console.log("Not leap year")
+// }
+
+// -----------question 7----------------
+
+
+// const speed=90;
+// if(speed>80){
+//     console.log("Over speed");
+// }
+// else{
+//     console.log("normal speed")
+// }
+
+//--------------question 8-----------------
+
+// let age =30;
+// let hasTicket=false;
+
+// if(age>18 && hasTicket){
+//     console.log("Entry Allowed")
+// }
+// else{
+//     console.log("not allowed")
+// }
+
+// -----------question 9-----------
+
+// let isWeekend =true;
+// let isHolyday=false;
+
+// if( isWeekend || isHolyday){
+
+//     console.log("No work today");
+// }
+
+// ----------question 10--------------
+
+// let userName=true;
+// let passWord=true;
+
+// if(userName && passWord){
+// console.log("Login successful")
+// }
+
+// --------------question 11-----------------
+
+// let marks=30;
+
+// if(marks>80){
+//     console.log("A+")
+// }
+// else if (marks>70){
+//     console.log("A")
+// }
+// else if(marks>60){
+//     console.log("B")
+// }
+// else if(marks>50){
+//     console.log("C")
+// }
+// else if(marks>40){
+//     console.log("D")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// -------question 12------------
+
+// let bmi=20;
+
+// if(bmi<18.5){
+//     console.log("Underweight");
+// }
+// else if(bmi>18.5){
+//     console.log("Normal weight");
+// }
+// else if(bmi>24.9){
+//     console.log("Overweight")
+// }
+// else if(bmi>29.9){
+//     console.log("Obesity (Class I)")
+// }
+// else if(bmi>34.9){
+//     console.log("Obesity (Class II")
+// }
+// else if(bmi>39.9){
+//     console.log("Obesity (Class III / Severe Obesity)")
+// }
+
+// --------------question-13------------------
+
+// let month=13;
+
+// if(month==1||month==2||month==3||month==4){
+//     console.log("Winter");
+// }
+// else if(month==5||month==6||month==7||month==8){
+//     console.log("Summer");
+// }
+// else if(month==9||month==10||month==11||month==12){
+//     console.log("Monsson")
+// }
+// else{
+//     console.log("Error")
+// }
+// --------------question 14-------------
+
+// let age =20;
+// let hasNID= false;
+
+// if(age>18){
+  
+//     if(hasNID){
+//         console.log("You can vote")
+
+//     }
+//     else{
+//         console.log("NID Required")
+//     }
+// }
+// else{
+//     console.log("Under 18")
+// }
+
+// ----------------question 15---------------
+
+// let isLogedin=false;
+// let isAdmin=true;
+
+// if(isLogedin){
+//     if(isAdmin){
+//         console.log("Admin Dashboard");
+//     }
+//     else{
+//         console.log("User Dashboard")
+//     }
+// }
+// else{
+//     console.log("please login ")
+// }
+
+// ----------question 16------------------
+
+// let number =13;
+
+//  number%2===0? console.log("even"):console.log("odd")
+
+//  -------------question 17--------------------
+
+// const age=17;
+
+// age>18? console.log("Adult"):console.log("Minor")
+ 
+// --------------question 18------------------------
+
+// let price=7000;
+
+// price>1000? console.log("Expensive"):console.log("Effordable")
